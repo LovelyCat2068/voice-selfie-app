19829049954c4ef4e6195f20365cddeaf4c75970
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-07-02 20:49:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,30 @@ recognition.onresult = function(event){
 
     animagus = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = animagus;
+    if(animagus == "Take my selfie."){
+        console.log("take my selfie");
+        audio();
+    }
 }
+
+function audio(){
+    var synth = window.speechSynthesis;
+
+    var speak_data = "Your Selfie Will Be Taken In 2 Sec";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+
+camera = document.getElementById("camera");
+
+Webcam.set({
+    width: 350,
+    height: 350,
+    image_format: 'jpeg',
+    jpeg_quality: 100
+});
+
